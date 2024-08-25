54152d0795bcd22c04fd8cc109c5c0d719370a2d
burskyor <ludmila.rohac@gmail.com>
2024-08-25 18:23:47 +0300

    Add files via upload

---
+++ def multiplication_or_sum(num1, num2):.py
@@ -0,0 +1,12 @@
+Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
+
+Given:
+
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+Expected Output:
+
+result list: [25, 35, 40, 60, 90]
+
+def odd_numbers(list1, list 2)
+print("result list:", )
